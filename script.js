fd0904522b88d109e1a7ca8e87150626f7961875
TTP-2 <f15225410@gmail.com>
2024-11-20 17:47:04 +1300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function reply() {
-  const userInput = document.getElementById('')
+  const userInput = document.getElementById('userInput').Value.toUpperCase();
+  const responce = document.getElementById('responce');
+
+  let responce;
+  if (userInput.includes('hi')) {
+    responce = "Hello, I hope your well!";
+  }
+  else if (userInput.includes('how are you')) {
+    respoce = "I'm doing very well thank you"
+  }
 }
