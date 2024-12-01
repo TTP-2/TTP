921fee840df2f2ced4d1f7b47cf82c67d1867a41
TTP-2 <f15225410@gmail.com>
2024-12-01 14:43:59 +1300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname + '/public')); // Serve static files
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('message', (data) => {
+        io.emit('message', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
