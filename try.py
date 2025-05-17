93d2baeee284049a537b40888169d21a9837ae02
TTP <f15225410@gmail.com>
2025-05-18 08:09:41 +1200

    Update and rename try.html to try.py

---
+++ try.py
@@ -0,0 +1,11 @@
+# This program adds two numbers
+
+# Ask the user for two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the numbers
+sum = num1 + num2
+
+# Show the result
+print("The sum is:", sum)
